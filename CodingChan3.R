741c3ba457d624cb4bb54482cde6d33018f7e008
Damseltemi <111428859+Damseltemi@users.noreply.github.com>
2025-02-20 13:40:50 -0600

    Coding Challenge 3

--- CodingChan3.R
+++ CodingChan3.R
@@ -68,39 +68,26 @@ figure_comb <- ggarrange(
 figure_comb
 #question 5
 
-#question2
-Plot1a <- ggplot(data1, aes(x = Treatment, y = DON, fill = Cultivar)) +
-  geom_boxplot(position =  "dodge") +  # Boxplot with proper dodge width
-  geom_jitter( pch = 21, position = position_jitterdodge(),color = "black") + # Adjusted jitter dodge
-  xlab("") +
-  ylab("DON (ppm)") +
-  theme_classic() +
-  geom_jitter(pch=21,alpha=0.6,position = position_jitterdodge(),color="black")+
-  scale_fill_manual(values = c(cbbPalette[[3]], cbbPalette[[4]]) ) +
-  scale_shape_manual(values = c(15, 16, 17, 18), name = "", labels = c("NTC", "Fg", "Fg +37", "Fg +40", "Fg +70")) +
-  facet_wrap(~Cultivar, scales = "free")
+### example with * as significance levels
+Plot1a <- Plot1 + 
+  geom_pwc(aes(group = Cultivar), method = "t_test", label = "p.adj.signif")
 Plot1a
-#question3
-Plot2a <- ggplot(data1, aes(x = Treatment, y = X15ADON, fill = Cultivar)) +
-  geom_boxplot(position =  "dodge") +  # Boxplot with proper dodge width
-  geom_jitter( pch = 21, position = position_jitterdodge(),color = "black") + # Adjusted jitter dodge
-  xlab("") +
-  ylab("15ADON") +
-  theme_classic() +
-  geom_jitter(pch=21,alpha=0.6,position = position_jitterdodge(),color="black")+
-  scale_fill_manual(values = c(cbbPalette[[3]], cbbPalette[[4]]) ) +
-  scale_shape_manual(values = c(15, 16, 17, 18), name = "", labels = c("NTC", "Fg", "Fg +37", "Fg +40", "Fg +70")) +
-  facet_wrap(~Cultivar, scales = "free")
+### Example with combined pvalue and * to indicate significance
+Plot2a <- Plot2 + 
+  geom_pwc(aes(group = Cultivar), method = "t_test", label = "{p.adj.format}{p.adj.signif}")
 Plot2a
-#question4a
-Plot3a<-ggplot(data1, aes(x = Treatment, y = MassperSeed_mg, fill = Cultivar)) +
-  geom_boxplot(position =  "dodge") +  # Boxplot with proper dodge width
-  geom_jitter( pch = 21, position = position_jitterdodge(),color = "black") + # Adjusted jitter dodge
-  xlab("") +
-  ylab("Seed Mass(mg)") +
-  theme_classic() +
-  geom_jitter(pch=21,alpha=0.6,position = position_jitterdodge(),color="black")+
-  scale_fill_manual(values = c(cbbPalette[[3]], cbbPalette[[4]]) ) +
-  scale_shape_manual(values = c(15, 16, 17, 18), name = "", labels = c("NTC", "Fg", "Fg +37", "Fg +40", "Fg +70")) +
-  facet_wrap(~Cultivar, scales = "free")
-Plot3a
+Plot3a <- Plot3 + 
+  geom_pwc(aes(group = Cultivar), method = "t_test", label = "{p.adj.format}{p.adj.signif}")
+
+
+figure_comb <- ggarrange(
+  Plot1,# First plot: water.imbibed
+  Plot2,
+  Plot3,  # Second plot: bac.even
+  labels = "auto",  # Automatically label the plots (A, B, C, etc.)
+  nrow = 3,  # Arrange the plots in 3 rows
+  ncol = 1, # Arrange the plots in 1 column
+  common.legend = TRUE
+  #legend = TRUE  # Do not include a legend in the combined figure
+)
+figure_comb
